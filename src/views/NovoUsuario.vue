a40c42a9e9ad03b3f1e498e88c16182efbbf21e2
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2023-12-14 23:11:25 -0300

    Feat: cadastro do usuário com endereço

--- src/views/NovoUsuario.vue
+++ src/views/NovoUsuario.vue
@@ -18,24 +18,35 @@
                             placeholder="Nome"
                             variante="v2"
                             v-model="usuario.nome"
+                            :erros="errosUsuario.nome"
                         />
                         <BaseInput 
                             nome="E-mail"
                             placeholder="E-mail"
                             variante="v2"
                             v-model="usuario.email"
+                            :erros="errosUsuario.email"
+                        />
+                        <BaseInput 
+                            nome="CPF"
+                            placeholder="CPF"
+                            variante="v2"
+                            v-model="usuario.cpf"
+                            :erros="errosUsuario.cpf"
                         />
                         <BaseInput 
                             nome="Telefone"
                             placeholder="Telefone"
                             variante="v2"
                             v-model="usuario.telefone"
+                            :erros="errosUsuario.telefone"
                         />
                         <BaseInput 
                             nome="Matrícula"
                             placeholder="Matrícula"
                             variante="v2"
                             v-model="usuario.matricula"
+                            :erros="errosUsuario.matricula"
                         />
                         <BaseInput
                             nome="Data de nascimento"
@@ -43,12 +54,13 @@
                             variante="v2"
                             tipo="date"
                             v-model="usuario.dataNascimento"
+                            :erro="errosUsuario.dataNascimento"
                         />
                         <BaseSwitch
-                            nome="Perfil"
                             opcao-esquerda="Aluno"
                             opcao-direita="Motorista"
                             v-model="usuario.perfil"
+                            :erros="errosUsuario.perfil"
                         />
                     </div>
                     <div class="w-full grid grid-cols-1 lg:grid-cols-2 gap-4 mt-8">
@@ -57,18 +69,21 @@
                             placeholder="Endereço"
                             variante="v2"
                             v-model="usuario.endereco.descricao"
+                            :erro="errosUsuario.endereco.descricao"
                         />
                         <BaseInput
                             nome="Número"
                             placeholder="Número"
                             variante="v2"
                             v-model="usuario.endereco.numero"
+                            :erro="errosUsuario.endereco.numero"
                         />
                         <BaseInput
                             nome="Bairro"
                             placeholder="Bairro"
                             variante="v2"
                             v-model="usuario.endereco.bairro"
+                            :erro="errosUsuario.endereco.bairro"
                         />
                         <BaseInput
                             nome="Cidade"
@@ -77,18 +92,21 @@
                             tipo="select"
                             :opcoes="cidades"
                             v-model="usuario.endereco.cidade"
+                            :erro="errosUsuario.endereco.cidade"
                         />
                         <BaseInput
                             nome="Complemento"
                             placeholder="Complemento"
                             variante="v2"
                             v-model="usuario.endereco.complemento"
+                            :erro="errosUsuario.endereco.complemento"
                         />
                         <BaseInput
                             nome="CEP"
                             placeholder="CEP"
                             variante="v2"
                             v-model="usuario.endereco.cep"
+                            :erro="errosUsuario.endereco.cep"
                         />
                         <BaseBtn 
                             class="mt-4 lg:w-2/3"
@@ -104,7 +122,9 @@
                             variante-cor="green"
                             @click="cadastrar"
                         >
-                            <PhPlus class="mr-1" /> Cadastrar
+                            <PhCircleNotch v-if="isCarregando" class="animate-spin" /> 
+                            <PhPlus v-else class="mr-1" />
+                            {{ isCarregando ? 'Cadastrando' : 'Cadastrar' }}
                         </BaseBtn>
                     </div>
                 </form>
@@ -121,12 +141,17 @@ import BaseBtn from '../components/form/BaseBtn.vue';
 import BaseInput from '../components/form/BaseInput.vue';
 import BaseSwitch from '../components/form/BaseSwitch.vue';
 import cidades from '../utils/cidades';
+import usuarioService from '../services/usuario-service';
+import apiConsumer from '../services/api-consumer';
 import { PhPlus, PhUserPlus, PhArrowLeft } from '@phosphor-icons/vue';
-import { reactive } from 'vue';
+import { ref, reactive } from 'vue';
+import { router } from '../routes';
+import { ErroValidacao } from '../utils/erros';
 
 const usuario = reactive({
     nome: '',
     email: '',
+    cpf: '',
     matricula: '',
     telefone: '',
     dataNascimento: '',
@@ -139,11 +164,77 @@ const usuario = reactive({
         complemento: '',
         cep: ''
     }
-})
+});
+
+const errosUsuario = reactive({
+    nome: [],
+    email: [],
+    cpf: [],
+    matricula: [],
+    telefone: [],
+    dataNascimento: [],
+    perfil: [],
+    endereco: {
+        rua: [],
+        numero: [],
+        bairro: [],
+        cidade: [],
+        complemento: [],
+        cep: []
+    }
+});
+
+const isCarregando = ref(false);
+const msgErro = ref("");
 
 async function cadastrar() {
-    console.log("cadastrando??");
-    console.log(usuario.endereco.cidade);
+    isCarregando.value = true;
+    limparErros();
+
+    try {
+        usuario.perfil = usuario.perfil.toUpperCase()
+        await usuarioService.cadastrarUsuario(usuario);
+
+        router.back();
+
+    } catch (erro) {
+        console.log(erro);
+
+        if (erro instanceof ErroValidacao) {
+            handleErroValidacao(erro);
+            return;
+        }
+
+        msgErro.value = erro.message;
+
+    } finally {
+        isCarregando.value = false;
+    }
+}
+
+function handleErroValidacao(erro) {
+    for (const { campo, mensagem } of erro.campos) {
+        if (errosUsuario[campo]) {
+            errosUsuario[campo].push(mensagem);
+        } else {
+            errosUsuario.endereco[campo].push(mensagem);
+        }
+    }
+}
+
+function limparErros() {
+    msgErro.value = "";
+
+    for (const campo in errosUsuario) {
+        if (campo instanceof Array) {
+            for (const subCampo in campo) {
+                errosUsuario[campo][subCampo] = [];
+            }
+
+        } else {
+            errosUsuario[campo] = [];
+        }
+    }
 }
 
 </script>
