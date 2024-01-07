9101a1654e2fafdf3a693783255d1d4e336bed2a
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2024-01-06 22:14:32 -0300

    Fix: nome da props opcaoEsquerda no BaseSwitch.vue

--- src/components/form/BaseSwitch.vue
+++ src/components/form/BaseSwitch.vue
@@ -43,7 +43,7 @@ const props = defineProps({
     },
     opcaoEsquerda: {
         type: String,
-        defult: "Não",
+        default: "Não",
     },
     opcaoDireita: {
         type: String,
