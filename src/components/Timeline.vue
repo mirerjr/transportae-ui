87b2a27e0f1a9b1106469b4e389469f2a50ef624
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2024-01-06 22:14:54 -0300

    Feat: cria componente para construir timelines

---
+++ src/components/Timeline.vue
@@ -0,0 +1,20 @@
+<template>
+    <h4 
+        v-if="props.titulo"
+        class="text-xl text-gray-700 font-bold"
+    >
+        {{ props.titulo }}
+    </h4>
+    <div class="relative px-2.5">
+        <div class="absolute h-full border border-dashed ml-[0.2rem] border-gray-400"></div>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    titulo: {
+        type: String,
+    },
+})
+</script>
---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,53 @@
+<template>
+     <div class="flex items-center w-full my-6 -ml-1.5">
+        <div class="w-1/12 mr-2 z-10">
+            <div class="w-5 h-5 bg-cyan-500 rounded-full flex items-center justify-center">
+                <component 
+                    v-if="props.icone"
+                    :is="props.icone"
+                    class="text-cyan-700 border rounded-full border-white bg-white"
+                />
+                <div
+                    v-else
+                    class="rounded-full"
+                    :class="{'w-2.5 h-2.5 bg-white': props.variante == 'v1',
+                            'w-3 h-3 border-2 bg-white border-cyan-600': props.variante == 'v2',
+                            'w-2.5 h-2.5 bg-cyan-600': props.variante == 'v3',
+                    }"
+                >
+                </div>
+            </div>                      
+        </div>
+        <div class="w-11/12">
+            <p class="text-sm">{{ props?.titulo }}</p>
+            <p v-if="props.conteudo" class="text-xs text-gray-500">
+                {{ props?.conteudo }}
+            </p>
+            <p v-else class="text-xs text-gray-500">
+                <slot></slot>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    titulo: {
+        type: String,
+        required: true
+    },
+    conteudo: {
+        type: String,
+        required: false
+    },
+    icone: {
+        required: false
+    },
+    variante: {
+        type: String,
+        required: false,
+        default: 'v1',
+        validator: (value) => ['v1', 'v2', 'v3'].includes(value)
+    },
+})
+</script>
